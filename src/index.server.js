b6bf615d67390c048a11675628f436b0fcf21fb5
mohamed bebba <mohamedbebba1@gmail.com>
2022-07-28 21:26:06 +0200

    add user Schema

--- src/index.server.js
+++ src/index.server.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express")
 require("dotenv").config();
 const app = express();
@@ -6,7 +7,7 @@ const database =  require("./database/database")
 // connection database
 database();
 
-
+app.use(bodyParser());
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on Port ${process.env.PORT}`)
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -2,11 +2,46 @@ const mongoose = require("mongoose");
 const Schema =  mongoose.Schema;
 
 const userSchema =  new Schema({
-userName:{
-    type: String,
-    required :["Please enter a username ",true],
-}
-
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20,
+      },
+      lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20,
+      },
+      username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+      },
+      hash_password: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: ["user", "admin", "super-admin"],
+        default: "user",
+      },
+      contactNumber: { type: String },
+      pofilePicture: { type: String },
 },
 {
     timestamps: true
