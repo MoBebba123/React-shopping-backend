a3c87fd34e6991fcef407a45683e81aea9f294a5
mohamed bebba <mohamedbebba1@gmail.com>
2022-08-13 21:56:41 +0200

    new changes

---
+++ src/models/itemCategory.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  merchant: { type: Schema.Types.ObjectId, required: true, ref: "Merchant" },
+  name: { type: String, required: true },
+  items: { type: [Schema.Types.ObjectId], default: [], ref: "Merchant_item" },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
